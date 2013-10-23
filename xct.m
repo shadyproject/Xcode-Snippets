6c9ae386ea9b680ff69b5152e7df19ab16309fc5
shadyproject <shady@shadyproject.net>
2013-10-23 09:05:37 -0700

    test case and assert fail snippets

---
+++ xct.m
@@ -0,0 +1,10 @@
+// XCT Test Method
+// Stub method for xct test
+//
+// Platform: All
+// Language: Objective-C
+// Completion Scope: Class Definition
+
+-(void)test<#Test Name#>{
+  <#XCTFail(@"Implement Me: \"%@\"", __PRETTY_FUNCTION__);#>
+}
---
+++ xf.m
@@ -0,0 +1,8 @@
+// XCT Fail
+// Assert failure
+//
+// Platform: All
+// Language: Objective-C
+// Completion Scope: Class body
+
+XCTFail(<#@"TODO Implement Me: \"%s\"", __PRETTY_FUNCTION__#>);
