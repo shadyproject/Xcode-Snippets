880794f9520b6baa736a3288ef16ecc697a24d73
shadyproject <shady@shadyproject.net>
2013-11-14 15:55:30 -0800

    predicate with format var snippet

---
+++ pwfvar.m
@@ -0,0 +1,8 @@
+// NSPredicate Varible
+// Create an NSPredicate via predicateWithFormat and a variable
+//
+// Platform: All
+// Language: Objective-C
+// Completion Scope: function or method
+
+NSPredicate *<#variable name#> = [NSPredicate predicateWithFormat:@"<#format#>", <#format vars#>];
