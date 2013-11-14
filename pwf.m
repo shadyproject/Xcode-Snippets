688e1e0b690491a7ec4b5edd3f92dd1eedc6fca4
shadyproject <shady@shadyproject.net>
2013-11-14 15:52:40 -0800

    added predicate with format

---
+++ pwf.m
@@ -0,0 +1,8 @@
+// NSPredicate
+// Create an NSPredicate via predicateWithFormat without a variable
+//
+// Platform: All
+// Language: Objective-C
+// Completion Scope: All
+
+[NSPredicate predicateWithFormat:@"<#format#>", <#format vars#>];
